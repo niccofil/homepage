cce552e0cc2d8392fce035af40c3428d649c571a
niccofil <nikita.ttu@gmail.com>
2021-05-01 23:33:00 +0300

    form

--- page.js
+++ page.js
@@ -5,6 +5,37 @@ function drop() {
     }
     else {
         toggle.style.display = 'none';
-    }
+    } 
 }
+
+function check(){
+    let user = document.getElementById('name').value;
+    let pass = document.getElementById('pass').value;
+
+    if (user == ""){
+        document.getElementById('emptyUser').innerHTML = "username required";
+        document.getElementById('emptyUser').style.color = 'red';
+        document.getElementById('name').style.borderColor = 'red';
+    }
+
+    
+    if (pass == "") {
+        document.getElementById('emptyPass').innerHTML = "password required";
+        document.getElementById('emptyPass').style.color = 'red';
+        document.getElementById('pass').style.borderColor = 'red';
+    }
+
+    else if (pass != "" && user != ""){
+        document.getElementById('emptyUser').innerHTML = "";
+        document.getElementById('name').style.borderColor = "";
+        document.getElementById('emptyPass').innerHTML = "";
+        document.getElementById('pass').style.borderColor = "";
+        drop(); 
+    }
+    
+}
+
+
+
+
 
