b30a81768086b0d5509ffb4adf856634bba0322b
niccofil <nikita.ttu@gmail.com>
2021-05-02 21:03:36 +0300

    merge

--- page.js
+++ page.js
@@ -1,10 +1,11 @@
 function drop() {
     let toggle = document.querySelector('.dropdown-conteiner');
-    if (toggle.style.display == 'none' || toggle.style.display == '') {
+    if (toggle.style.display == 'none'  || toggle.style.display == '') {
         toggle.style.display = 'block';
     }
     else {
         toggle.style.display = 'none';
     }
 }
+
 
